539158e575cb1cdc3927b20b895365248002ea5e
github-1-madhu <146877970+github-1-madhu@users.noreply.github.com>
2023-10-05 13:47:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,7 @@ streamlit.dataframe(my_data_rows)
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','jackfruit')
 streamlit.write('The user entered ', fruit_choice)
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "jackfruit")
 
 streamlit.write('Thanks for adding ', add_my_fruit)
